0195565244f0ba9bd7c57a9670acb2b7afd87033
Saurabh Khanduja <khandujasaurabh@gmail.com>
2023-08-21 14:39:02 +0200

    Move all inside src

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,16 @@
 module.exports = function(eleventyConfig) {
-    // Output directory: _site
-  
-    // Copy `css/` to `_site/css/`
+    // Copy `css/` to `docs/css/`
     // Keeps the same directory structure.
     eleventyConfig.addPassthroughCopy("css");
+
+    return {
+      // We can now use .html, .md and data files instead of having to use .njk files.
+      markdownTemplateEngine: 'njk',
+      dataTemplateEngine: 'njk',
+      htmlTemplateEngine: 'njk',
+      dir: {
+        input: "src",
+        output: "docs"
+      }
+    }
   };
